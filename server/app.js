2c984b13c46b9e8c0f2a164b74663fc195a3aacf
Uzeyr OZCAN <muzeyr@gmail.com>
2020-02-15 23:05:41 +0300

    Graphql ile kitap listesi,kitap detayı,kitap ekleme işlemleri

--- server/app.js
+++ server/app.js
@@ -3,11 +3,10 @@ const app = express();
 const graphqlHTTP = require('express-graphql');
 const schema = require('./schema/schema');
 const mongoose = require('mongoose');
+const cors = require('cors');
 
-//const uri = 'mongodb://muzeyr:m0320532@graphql-app-shard-00-00-qerqr.mongodb.net:27017/test?ssl=true&replicaSet=graphql-app-shard-0&authSource=admin&retryWrites=true&w=majority';
-//const ur =  'mongodb://muzeyr:m0320532@graphql-app-qerqr.mongodb.net/test?retryWrites=true&w=majority';
-const uri = 'mongodb://aosgames:aosgames1@ds161224.mlab.com:61224/udemy-gql-app';
-mongoose.connect(uri);
+app.use(cors());
+mongoose.connect('mongodb://muzer:m0320532@graphql-app-shard-00-00-qerqr.mongodb.net:27017,graphql-app-shard-00-01-qerqr.mongodb.net:27017,graphql-app-shard-00-02-qerqr.mongodb.net:27017/test?ssl=true&replicaSet=graphql-app-shard-0&authSource=admin&retryWrites=true&w=majority');
 mongoose.connection.once('open',()=>{
     console.log('başarılı');
 })
--- server/schema/schema.js
+++ server/schema/schema.js
@@ -5,10 +5,14 @@ const { GraphQLObjectType,
         GraphQLSchema,
         GraphQLID,
         GraphQLInt,
-        GraphQLList
-        }= graphql;
+        GraphQLList,
+        GraphQLNonNull
+}= graphql;
 
 const _ = require('lodash');
+
+const Kitap = require('../models/kitap');
+const Yazar = require('../models/yazar');
 
 var kitaplar = [
     {isim:'Harry Potter',tur:'Fantastik',id:'1',yazarId:'1'},
@@ -42,7 +46,9 @@ const KitapTip = new GraphQLObjectType({
         tur:{type:GraphQLString},
         yazar:{type:YazarTip,
             resolve(parent,args){
-                return _.find(yazarlar,{id:parent.yazarId});
+                //return _.find(yazarlar,{id:parent.yazarId});
+
+                return Yazar.findById(parent.yazarId);
 
             }
         }
@@ -58,7 +64,8 @@ const YazarTip = new GraphQLObjectType({
         kitaplar: {
             type: new GraphQLList(KitapTip),
             resolve(parent,args){
-                return _.filter(kitaplar,{yazarId: parent.id})
+                //return _.filter(kitaplar,{yazarId: parent.id})
+                return Kitap.find({yazarId:parent.id});
 
             }
         }
@@ -72,31 +79,80 @@ const RootQuery = new GraphQLObjectType({
             type:KitapTip,
             args:{id:{type:GraphQLID}},
             resolve(parent,args){
-                return _.find(kitaplar,{id:args.id});
+              //  return _.find(kitaplar,{id:args.id});
+                return Kitap.findById(args.id);
             }
         },
         yazar:{
             type:YazarTip,
             args:{id:{type:GraphQLID}},
             resolve(parent,args){
-                return _.find(yazarlar,{id:args.id});
+                //return _.find(yazarlar,{id:args.id});
+                return yazar.findById(args.id);
             }
         },
         kitaplar:{
             type:new GraphQLList(KitapTip),
             resolve(parent,args){
-                return kitaplar;
+                //return kitaplar;
+                return Kitap.find({});
             }
         },
         yazarlar:{
             type: new GraphQLList(YazarTip),
             resolve(parent,args){
-                return yazarlar;
+                return Yazar.find({});
             }
         }
     }
 });
 
+const Mutation = new GraphQLObjectType({
+    name: 'Mutation',
+    fields:{
+        yazarEkle:{
+            type:YazarTip,
+            args:{
+//                isim:{type:GraphQLString},
+//                yas:{type:GraphQLInt}
+                isim:{type: new GraphQLNonNull(GraphQLString)},
+                yas:{type: new GraphQLNonNull(GraphQLInt)},
+
+            },
+            resolve(parent,args){
+                let yazar = new Yazar({
+                    isim:args.isim,
+                    yas:args.yas,
+                });
+
+                return yazar.save();
+            }
+        },
+        kitapEkle:{
+            type:KitapTip,
+            args:{
+                //isim:{type:GraphQLString},
+                //tur:{type:GraphQLString},
+                //yazarId:{type:GraphQLID}
+                isim: {type:new GraphQLNonNull(GraphQLString)},
+                tur: {type:new GraphQLNonNull(GraphQLString)},
+                yazarId: {type:new GraphQLNonNull(GraphQLID)},
+
+            },
+            resolve(parent,args){
+                let kitap = new Kitap({
+                    isim:args.isim,
+                    tur:args.tur,
+                    yazarId:args.yazarId
+
+                });
+                return kitap.save();
+            }
+        }
+    }
+
+});
 module.exports = new GraphQLSchema({
-    query: RootQuery
+    query: RootQuery,
+    mutation: Mutation
 })
